7ef99cbe0ac3e8bf005e8cc08e13523f7917eb95
19PA1A05A2 <60594911+19PA1A05A2@users.noreply.github.com>
2021-12-13 16:30:28 +0530

    Update SnakesAndLadders.java

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -2,39 +2,51 @@ package SnakeAndLadder;
 import java.util.*;
 public class SnakesAndLadders {
         public static void main(String[] args){
-        Dice dice = new Dice(1);
-        Players A = new Players("Ram",1);
-        Players B = new Players("Ravi",2);
-        Players C = new Players("Siva",3);
-        Players D = new Players("Kiran",4);
-        Queue<Players> totalplayers = new LinkedList<>();
-        totalplayers.offer(A);
-        totalplayers.offer(B);
-        totalplayers.offer(C);
-        totalplayers.offer(D);
-        JumpPosition s1 = new JumpPosition(27,2);
-        JumpPosition s2 = new JumpPosition(99,18);
-        List<JumpPosition> snakes = new ArrayList<>();
-        snakes.add(s1);
-        snakes.add(s2);
-        JumpPosition l1 = new JumpPosition(21,92);
-        JumpPosition l2 = new JumpPosition(54,86);
-        List<JumpPosition> ladders = new ArrayList<>();
-        ladders.add(l1);
-        ladders.add(l2);
-        Map<String,Integer> playerPosition =new HashMap();
-        playerPosition.put("Ram",1);
-        playerPosition.put("Ravi",1);
-        playerPosition.put("Siva",1);
-        playerPosition.put("Kiran",1);
-        Game startPlay =new Game(dice,totalplayers,snakes,ladders,playerPosition,100);
-      
-        startPlay.startGame();
+            Scanner ob = new Scanner(System.in);
+            int noOfDice = ob.nextInt();
+            Dice dice = new Dice(noOfDice);
+            int playerSize=ob.nextInt();
+            String[] playersList = new String[playerSize];
+            int[] idList = new int[playerSize];
+            Queue<Players> totalplayers = new LinkedList<>();
+            List<JumpPosition> snakes = new ArrayList<>();
+            List<JumpPosition> ladders = new ArrayList<>();
+            for (int i=0;i<playerSize;i++){
+                idList[i]=ob.nextInt();
+                playersList[i]=ob.next();
+                Players x = new Players(playersList[i],idList[i]);
+                totalplayers.offer(x);
+            }
+            int noOfSnakes=ob.nextInt();
+            int[] sstart = new int[noOfSnakes];
+            int[] send = new int[noOfSnakes];
+            for (int j=0;j<noOfSnakes;j++){
+                sstart[j]=ob.nextInt();
+                send[j]=ob.nextInt();
+                JumpPosition s = new JumpPosition( sstart[j],send[j]);
+                snakes.add(s);
+            }
+            
+            int noOfLadders=ob.nextInt();
+            int[] lstart = new int[noOfLadders];
+            int[] lend = new int[noOfLadders];
+            for (int k=0;k<noOfLadders;k++){
+                lstart[k]=ob.nextInt();
+                lend[k]=ob.nextInt();
+                JumpPosition jp = new JumpPosition(lstart[k],lend[k]);
+                ladders.add(jp);
+             }
+            
+            Map<String,Integer> playerPosition =new HashMap<>();
+            for (int n=0;n<playerSize;n++){
+                playerPosition.put(playersList[n],1);
+                
+        }
+            Game startPlay =new Game(dice,totalplayers,snakes,ladders,playerPosition,100);
+            ob.close();
+
         
-
-
-
-
-    }
+            startPlay.startGame();
+        }
     
 }
